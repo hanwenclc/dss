df4755bc3dcd54000ab344be90e145500ba83bd9
hanwenclc <XU_Hanwen339@126.com>
2022-11-29 23:56:34 +0100

    q

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup()
+setup(
     name="snowflake",
     version="0.1",
     author="hanwenclc",
     author_email="XU_Hanwen339@126.com",
     packages=find_packages(),
     install_requires=["numpy","turtles"],
+)
