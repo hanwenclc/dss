12416d788e3cfb97481af37c77a21c7f0a1d4890
hanwenclc <XU_Hanwen339@126.com>
2022-11-29 23:51:05 +0100

    i

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(
-    name="snowflake_",
+setup()
+    name="snowflake",
     version="0.1",
-    auther="hanwenclc",
-    auther_email="XU_Hanwen339@126.com",
+    author="hanwenclc",
+    author_email="XU_Hanwen339@126.com",
     packages=find_packages(),
     install_requires=["numpy","turtles"],
-)
